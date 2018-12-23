899c67d8d8fa38bffaf0a3f4a8fabb0e24f0a5b9
Calvin-Liu <calvinliu411@gmail.com>
2018-12-22 23:04:29 -0800

    Switching from then to awaits. Also redirect working

--- scripts/puppet_request.js
+++ scripts/puppet_request.js
@@ -1,22 +1,24 @@
 const puppeteer = require('puppeteer');
-const url = 'https://www.delta.com/flight-search/search-results?cacheKeySuffix=9ee449d8-f7c0-49ba-b32f-975adbc74381';
+const url = 'https://www.delta.com/flight-search/search?action=findFlights&tripType=ROUND_TRIP&priceSchedule=PRICE&originCity=SJC&destinationCity=LAX&departureDate=01%2F10%2F2019&departureTime=AT&returnDate=01%2F15%2F2019&returnTime=AT&paxCount=1&searchByCabin=true&cabinFareClass=BE&deltaOnlySearch=false&deltaOnly=off&Go=Find%20Flights&meetingEventCode=&refundableFlightsOnly=false&compareAirport=false&awardTravel=false&datesFlexible=undefined&flexAirport=false&paxCounts%5B0%5D=1';
+const screenshot_options = {
+    path: 'screenshot.png',
+    fullPage: true
+}
 
-puppeteer
-    .launch()
-    .then(function(browser) {
-        return browser.newPage();
-    })
-    .then(function(page) {
-        page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36');
-        
-        return page.goto(url).then(function() {
-            page.screenshot({path: '../data/screenshot.png'});
-            return page.content();
-        });
-    })
-    .then(function(html) {
+puppeteer.launch().then(async browser => {
+    try {
+        const page = await browser.newPage();
+        await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36');
+        await page.goto(url);
+        await page.waitForSelector('span.priceBfrDec');
+        await page.screenshot(screenshot_options);
+        var html = await page.content(); 
         console.log(html);
-    })
-    .catch(function(err) {
-        console.log("Error getting webpage or executing JS on webpage");
-    });
+        await browser.close();
+    } catch(error) {
+        console.log("Error getting webpage or executing JS on webpage", error);
+    }
+})
+        
+
+    
