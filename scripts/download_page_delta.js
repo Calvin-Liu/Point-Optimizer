14bf816496a4a9e5b412e0c7f6b33b2e4afdeea5
Calvin-Liu <calvinliu411@gmail.com>
2018-12-24 13:01:50 -0800

    getUrl should return promise that can be called from other files

--- scripts/download_page_delta.js
+++ scripts/download_page_delta.js
@@ -2,4 +2,16 @@ var request = require('request');
 var cheerio = require('cheerio');
 let delta_html = require('./puppet_request');
 
-delta_html.getUrl();
+async function parse() {
+  var html = await delta_html.getUrl();
+  console.log(html);
+  //var $ = cherrio.load(html);
+  //var parseResults = [];
+  //$('span.tblCntBigTxt').each(function(i, element) {
+  //  var spanOfDollar = $(this).children('priceBfrDec').text();
+  //})
+}
+
+parse();
+
+
--- scripts/puppet_request.js
+++ scripts/puppet_request.js
@@ -5,24 +5,26 @@ const screenshot_options = {
     fullPage: true
 }
 
-function getUrl() {
-    puppeteer.launch().then(async browser => {
-        try {
-            const page = await browser.newPage();
-            await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36');
-            await page.goto(url);
-            await page.waitForSelector('span.priceBfrDec');
-            await page.screenshot(screenshot_options);
-            var html = await page.content(); 
-            console.log(html);
-            await browser.close();
-        } catch(error) {
-            console.log("Error getting webpage or executing JS on webpage", error);
-        }
-    });
+async function getUrl() {
+    return new Promise(function(resolve, reject) {
+        puppeteer.launch().then(async browser => {
+            try {
+                const page = await browser.newPage();
+                await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36');
+                await page.goto(url);
+                await page.waitForSelector('span.priceBfrDec');
+                await page.screenshot(screenshot_options);
+                var html = await page.content(); 
+                await browser.close();
+                resolve(html);
+            } catch(error) {
+                console.log("Error getting webpage or executing JS on webpage", error);
+            }
+        });
+    })
+    
 }
-        
-
+    
 module.exports.getUrl = getUrl;
 
     
