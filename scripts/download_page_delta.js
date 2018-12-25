6a7bd27e8aa517671336f617059e912b5366a7ba
Calvin-Liu <calvinliu411@gmail.com>
2018-12-24 18:00:53 -0800

    Able to parse price

--- scripts/download_page_delta.js
+++ scripts/download_page_delta.js
@@ -1,15 +1,21 @@
-var request = require('request');
 var cheerio = require('cheerio');
 let delta_html = require('./puppet_request');
 
 async function parse() {
-  var html = await delta_html.getUrl();
-  console.log(html);
-  //var $ = cherrio.load(html);
-  //var parseResults = [];
-  //$('span.tblCntBigTxt').each(function(i, element) {
-  //  var spanOfDollar = $(this).children('priceBfrDec').text();
-  //})
+	var html = await delta_html.getUrl();
+	//console.log(html);
+	var $ = cheerio.load(html);
+	var results = [];
+	$('.priceBfrDec').each(function () {
+		var price = $(this).text();
+		var metadata = {
+			price: parseInt(price)
+		};
+
+		results.push(metadata);
+	});
+
+	console.log(results)
 }
 
 parse();
