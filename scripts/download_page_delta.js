4ae8c49a9e5e01919bed6eac95dcba831e325474
Calvin-Liu <calvinliu411@gmail.com>
2019-01-06 12:44:29 -0800

    Refactored for sold out tickets

--- scripts/download_page_delta.js
+++ scripts/download_page_delta.js
@@ -5,22 +5,41 @@ async function parseMoney() {
 	var html = await delta_html.getUrl();
 	var $ = cheerio.load(html);
 	var results = [];
-	$('.priceBfrDec').each(function () {
-		var price = $(this).text();
-		var flightType = $(this).parent().parent().prev()
-			.children()
+	$('.fiveCabinClass.fareClassBody ').each(function () {
+		// var price = $(this).text();
+
+
+		var metadata;
+		var td = $(this);
+		var cabinInfoHolder = td.children();
+		var cabinTitle = cabinInfoHolder.children('div.cabinTitle');
+		var priceHolder = cabinInfoHolder.children('div.priceHolder');
+		var flightType = cabinTitle.children()
 			.children('span.dl-modal-component-calling-element')
 			.children("a.lnkCabinName")
 			.children()
 			.first().text();
+		var price = priceHolder.children().children('span.priceBfrDec').text();
 
-		var metadata = {
-			airline: 'Delta',
-			flightType: flightType,
-			price: parseInt(price)
-		};
+		if (cabinInfoHolder.hasClass('cabinNaPriceHolder')) {
+			metadata = {
+				airline: 'Delta',
+				flightType: flightType,
+				price: 'Sold Out'
+			};
+		} else if (cabinInfoHolder.hasClass('cabinDetailsHolder')) {
+
+			metadata = {
+				airline: 'Delta',
+				flightType: flightType,
+				price: parseInt(price)
+			};
+		} else {
+			console.log('No cabinNaPriceHolder or cabinDetailsHolder class found');
+		}
 
 		results.push(metadata);
+
 	});
 
 	console.log(results)
@@ -38,7 +57,8 @@ async function parseMiles() {
 
 }
 
-var flightInfo = parseMoney();
+//var flightInfo = parseMoney();
+parseMoney();
 
 
 
